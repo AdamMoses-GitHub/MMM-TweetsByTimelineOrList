74d4fe7c2a0f2edcefe53cdf92990e8b91113593
N S <neil.scott2@virgin.net>
2020-03-11 10:34:05 +0000

    Added HashTag Search Option

--- MMM-TweetsByTimelineOrList.js
+++ MMM-TweetsByTimelineOrList.js
@@ -12,15 +12,15 @@
  */
 
 Module.register("MMM-TweetsByTimelineOrList", {
-	// setup the default config options
-	defaults: {		
-		// required
-		consumer_key: null,
-		consumer_secret: null,
-		access_token_key: null,
-		access_token_secret: null,
+    // setup the default config options
+    defaults: {
+        // required
+        consumer_key: null,
+        consumer_secret: null,
+        access_token_key: null,
+        access_token_secret: null,
         screenName: null,
-        listToShow: 'TIMELINE',
+        listToShow: 'TIMELINE', //SEARCH
         // optional
         tweetsToShowAtATime: 5,
         onScreenRefreshRate: 25 * 1000,
@@ -28,67 +28,66 @@ Module.register("MMM-TweetsByTimelineOrList", {
         moduleWidth: "300px",
         animationSpeed: 3 * 1000,
         showHeader: false,
-        totalTweetsPerUpdate: 150,
-        excludeTweetsWithQuotes: true,
-        excludeRetweets: true, 
+        totalTweetsPerUpdate: 25,
+        excludeTweetsWithQuotes: false,
+        excludeRetweets: true,
         excludeMediaTweets: false,
         excludeLinkTweets: false,
         excludeTweetLengthLessThan: 16,
-        excludeTweetsWithoutText: [ ],
-        maxTweetsPerUser: 1,
+        excludeTweetsWithoutText: [],
+        maxTweetsPerUser: 10,
         maxTweetAgeMins: 360,
-		allowSpecialCharacters: false,
-		displayColors: [ '#888', '#aaa', ],
-	},
-	// the start function
-	start: function() {
-		// log starting
-		Log.info("Starting module: " + this.name);
-		// check refresh rate is not faster than 1 minute
-		this.config.refreshRate = Math.max(60, this.config.tweetUpdateRefreshRate);
-		// set loaded, error, and the update to init values
-		this.loaded = false;
-		this.errorMessage = null;
-		this.tweetList = null;
+        allowSpecialCharacters: false,
+        displayColors: ['#888', '#aaa',],
+    },
+    // the start function
+    start: function () {
+        // log starting
+        Log.info("Starting module: " + this.name);
+        // check refresh rate is not faster than 1 minute
+        this.config.refreshRate = Math.max(60, this.config.tweetUpdateRefreshRate);
+        // set loaded, error, and the update to init values
+        this.loaded = false;
+        this.errorMessage = null;
+        this.tweetList = null;
         this.tweetIndex = null;
         this.config.identifier = this.identifier;
         this.updateTimer = null;
-		// set the header to this place
-        if (this.config.showHeader) {    
+        // set the header to this place
+        if (this.config.showHeader) {
             this.data.header = this.config.screenName + " - " + this.config.listToShow;
         }
         // if not missing required config fields start things up
-		if ( (this.config.consumer_key != null) && (this.config.consumer_secret != null)
-				&& (this.config.access_token_key != null) && (this.config.access_token_secret != null) 
-                && (this.config.screenName != null) ) {
-			// add this config to the helper functions
-			this.sendSocketNotification('TWEETS_REGISTER_CONFIG', this.config);
-		}
+        if ((this.config.consumer_key != null) && (this.config.consumer_secret != null)
+            && (this.config.access_token_key != null) && (this.config.access_token_secret != null)
+            && (this.config.screenName != null)) {
+            // add this config to the helper functions
+            this.sendSocketNotification('TWEETS_REGISTER_CONFIG', this.config);
+        }
         // otherwise set error message
-		else {
-			this.error = "Required config missing.";
-		}		
-	},
-	// the socket handler
-	socketNotificationReceived: function(notification, payload) {
-		// if an update was received
-		if (notification === "TWEETS_UPDATE") {
-			// check this is for this module based on the woeid
-			if (payload.identifier === this.identifier)
-			{
-				// set loaded flag, set the update, and call update dom                
-				this.tweetList = payload.tweetList;
+        else {
+            this.error = "Required config missing.";
+        }
+    },
+    // the socket handler
+    socketNotificationReceived: function (notification, payload) {
+        // if an update was received
+        if (notification === "TWEETS_UPDATE") {
+            // check this is for this module based on the woeid
+            if (payload.identifier === this.identifier) {
+                // set loaded flag, set the update, and call update dom                
+                this.tweetList = payload.tweetList;
                 this.tweetIndex = 0;
                 if (!this.loaded) {
                     this.loaded = true;
                     this.updateDom(this.config.animationSpeed);
                     var self = this;
                     this.updateTimer = setInterval(
-                            function() { self.updateDom(self.config.animationSpeed); }, 
-                            this.config.onScreenRefreshRate );
+                        function () { self.updateDom(self.config.animationSpeed); },
+                        this.config.onScreenRefreshRate);
                 }
-			}
-		}
+            }
+        }
         // if sent error notice
         if (notification === "TWEETS_TOO_MANY_ERRORS") {
             this.errorMessage = "There was an error.";
@@ -97,14 +96,14 @@ Module.register("MMM-TweetsByTimelineOrList", {
             this.updateTimer = null;
             this.updateDom();
         }
-	},
+    },
     // gathers the current set of tweets from the full list to display
-    getTweetsToShow: function() {
+    getTweetsToShow: function () {
         if (this.tweetList.length <= this.config.tweetsToShowAtATime)
             return this.tweetList;
         var indexStart = this.tweetIndex % this.tweetList.length;
         var indexEnd = indexStart + this.config.tweetsToShowAtATime - 1;
-        var returnTweets = [ ];
+        var returnTweets = [];
         if (indexEnd < this.tweetList.length) {
             returnTweets = this.tweetList.slice(indexStart, indexEnd + 1);
         }
@@ -130,51 +129,51 @@ Module.register("MMM-TweetsByTimelineOrList", {
             return diffMins + "m";
         }
         var diffHours = Math.round(diffSecs / (60 * 60));
-        return diffHours + "h";      
+        return diffHours + "h";
     },
-	// the get dom handler
-	getDom: function() {
+    // the get dom handler
+    getDom: function () {
         // if an error, say so
         if (this.errorMessage !== null) {
             var wrapper = document.createElement("div");
-			wrapper.className = "small";
-			wrapper.innerHTML = this.errorMessage;
-			return wrapper;	
+            wrapper.className = "small";
+            wrapper.innerHTML = this.errorMessage;
+            return wrapper;
         }
-		// if nothing loaded yet, put in placeholder text
-		if (!this.loaded) {
-			var wrapper = document.createElement("div");
-			wrapper.className = "small";
-			wrapper.innerHTML = "Awaiting Update...";
-			return wrapper;			
-		}
-		var wrapper = document.createElement("table");
-		var currentDisplayTweets = this.getTweetsToShow();
-		for (var cIndex = 0; cIndex < currentDisplayTweets.length; cIndex++){
+        // if nothing loaded yet, put in placeholder text
+        if (!this.loaded) {
+            var wrapper = document.createElement("div");
+            wrapper.className = "small";
+            wrapper.innerHTML = "Awaiting Update...";
+            return wrapper;
+        }
+        var wrapper = document.createElement("table");
+        var currentDisplayTweets = this.getTweetsToShow();
+        for (var cIndex = 0; cIndex < currentDisplayTweets.length; cIndex++) {
             var tweet = currentDisplayTweets[cIndex];
-            var colorValue = "color:" + 
-							this.config.displayColors[cIndex % this.config.displayColors.length];
-			var tweetTR = document.createElement("tr");    
-   			var tweetTD = document.createElement("td");
-  			tweetTD.align = "right";
-			tweetTD.className = "xsmall";
-            tweetTD.style = colorValue;	
+            var colorValue = "color:" +
+                this.config.displayColors[cIndex % this.config.displayColors.length];
+            var tweetTR = document.createElement("tr");
+            var tweetTD = document.createElement("td");
+            tweetTD.align = "right";
+            tweetTD.className = "xsmall";
+            tweetTD.style = colorValue;
             tweetTD.width = this.config.moduleWidth;
-			tweetTD.innerHTML = tweet.name + ' - ' + this.getStringTimeDifference(tweet.timestamp);
-			tweetTR.appendChild(tweetTD);            
-			var usertimeTR = document.createElement("tr");
-			var usertimeTD = document.createElement("td");
-			usertimeTD.align = "left";
-			usertimeTD.className = "small";
+            tweetTD.innerHTML = tweet.name + ' - ' + this.getStringTimeDifference(tweet.timestamp);
+            tweetTR.appendChild(tweetTD);
+            var usertimeTR = document.createElement("tr");
+            var usertimeTD = document.createElement("td");
+            usertimeTD.align = "left";
+            usertimeTD.className = "small";
             usertimeTD.style = colorValue;
-			usertimeTD.width = this.config.moduleWidth;
-			usertimeTD.innerHTML = tweet.text;
-			usertimeTR.appendChild(usertimeTD);
-			wrapper.appendChild(usertimeTR);
-			wrapper.appendChild(tweetTR);			
-		}
-		return wrapper;	
-	}
+            usertimeTD.width = this.config.moduleWidth;
+            usertimeTD.innerHTML = tweet.text;
+            usertimeTR.appendChild(usertimeTD);
+            wrapper.appendChild(usertimeTR);
+            wrapper.appendChild(tweetTR);
+        }
+        return wrapper;
+    }
 });
 
 // ------------ end -------------
--- node_helper.js
+++ node_helper.js
@@ -20,7 +20,8 @@ var filter = new Filter();
 // the main module helper create
 module.exports = NodeHelper.create({
     // subclass start method, clears the initial config array
-    start: function() {
+    start: function () {
+        console.log(this.name + " #### loaded node helper");
         this.moduleConfigs = [];
         this.timers = [];
         this.errorCount = 0;
@@ -28,37 +29,36 @@ module.exports = NodeHelper.create({
         this.errorFailLimit = 10;
     },
     // subclass socketNotificationReceived, received notification from module
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === "TWEETS_REGISTER_CONFIG") {              
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "TWEETS_REGISTER_CONFIG") {
             // add the current config to an array of all configs used by the helper
             this.moduleConfigs[this.moduleConfigs.length] = payload;
             // this to self
-            var self = this;     
+            var self = this;
             // call the initial update now
             this.updateTweets(payload);
             // schedule the updates
             this.timers[this.timers.length] = setInterval(
-                function() { self.updateTweets(payload); }, payload.tweetUpdateRefreshRate);
+                function () { self.updateTweets(payload); }, payload.tweetUpdateRefreshRate);
         }
     },
     // increment error count, if passed limit send notice to module and stop updates
-    processError: function() {
+    processError: function () {
         this.errorCount += 1;
-        if (this.errorCount >= this.errorFailLimit)
-        {
-            this.sendSocketNotification('TWEETS_TOO_MANY_ERRORS', {} );
+        if (this.errorCount >= this.errorFailLimit) {
+            this.sendSocketNotification('TWEETS_TOO_MANY_ERRORS', {});
             for (var cIndex = 0; cIndex < this.timers.length; cIndex++)
                 clearTimeout(this.timers[cIndex]);
             this.timers = [];
         }
-    },    
+    },
     // return clean tweet text / name
     // removes all urls, newlines, eol :'s, and multi-spaces
-    cleanString: function(theString, theConfig) {
+    cleanString: function (theString, theConfig) {
         var cTextClean = theString;
-        cTextClean = cTextClean.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');          
+        cTextClean = cTextClean.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
         if (!theConfig.allowSpecialCharacters)
-			cTextClean = cTextClean.replace(/[^\x00-\x7F]/g, '');
+            cTextClean = cTextClean.replace(/[^\x00-\x7F]/g, '');
         cTextClean = cTextClean.replace(/\n/g, ' ');
         cTextClean = cTextClean.replace(/\s+/g, ' ');
         cTextClean = cTextClean.trim();
@@ -70,129 +70,174 @@ module.exports = NodeHelper.create({
     },
     // checks that a given tweet's text has the config text match met
     // uses the excludeTweetsWithoutText config array option
-    doesNotHaveRequiredText: function(theText, theConfig) {
-    	var theTextL = theText.toLowerCase();
-    	var excludeTweetsWithoutText = theConfig.excludeTweetsWithoutText;
-    	for (cIndex = 0; cIndex < excludeTweetsWithoutText.length; cIndex++) {
-    		var cTextMatch = excludeTweetsWithoutText[cIndex].toLowerCase();
-    		if (theTextL.indexOf(cTextMatch) > -1)
-    			return false;
-    	}
-    	return true;
+    doesNotHaveRequiredText: function (theText, theConfig) {
+        var theTextL = theText.toLowerCase();
+        var excludeTweetsWithoutText = theConfig.excludeTweetsWithoutText;
+        for (cIndex = 0; cIndex < excludeTweetsWithoutText.length; cIndex++) {
+            var cTextMatch = excludeTweetsWithoutText[cIndex].toLowerCase();
+            if (theTextL.indexOf(cTextMatch) > -1)
+                return false;
+        }
+        return true;
     },
     // parses the received tweets and send back to module
-    parseTweets: function(theConfig, tweets) {
-          var includedTweetList = [ ];          
-          var userTweetCountList = {};
-          var nowTime = Date.now();
-          for (var cIndex = 0; cIndex < tweets.length; cIndex++)
-          {
-              // break out all the tweet components
-              var cTweet = tweets[cIndex];
-              var cText = this.cleanString(cTweet.text, theConfig);
-              var cTimestamp = cTweet.created_at;
-              var cTweetAgeMins = Math.round((nowTime - Date.parse(cTimestamp)) / (1000 * 60));
-              var cUserFullName = this.cleanString(cTweet.user.name, theConfig);
-              var cUserScreenName = cTweet.user.screen_name;
-              var cIsQuoteStatus = cTweet.is_quote_status;
-              var cIsRetweeted = (cTweet.retweeted_status !== undefined);
-              var cHasMedia = (cTweet.entities.media !== undefined);
-              var cHasURLs = (cTweet.entities.urls.length !== 0);
-              if (userTweetCountList[cUserScreenName] === undefined)
-                    userTweetCountList[cUserScreenName] = { count: 1 };                     
-                else
-                    userTweetCountList[cUserScreenName].count += 1;                          
-              var cUserTweetCount = userTweetCountList[cUserScreenName].count;
-               // set flag to assume inclusion of this tweet
-              var doInclude = true;
-              // if set to exclude quote and has a quote, exclude
-              if (theConfig.excludeTweetsWithQuotes && cIsQuoteStatus) 
-                  doInclude = false;
-              // if set to exclude retweets and is a retweet, exclude
-              if (theConfig.excludeRetweets && cIsRetweeted)
-                  doInclude = false;
-              // if set to exclude tweets with media has media, exclude
-              if (theConfig.excludeMediaTweets && cHasMedia)
-                  doInclude = false;
-              // if set to exclude tweets with links and has a link, exclude
-              if (theConfig.excludeLinkTweets && cHasURLs)
-                  doInclude = false;
-              // if set to exclude short tweets and is short, exclude
-              if ( (theConfig.excludeTweetLengthLessThan > 0) && 
-                    (cText.length < theConfig.excludeTweetLengthLessThan) )
-                    doInclude = false;
-              // if set to limit tweets per user check if exceeds that amount, exclude
-              if ( (theConfig.maxTweetsPerUser > 0) && 
-                    (cUserTweetCount > theConfig.maxTweetsPerUser) )
-                    doInclude = false;
-              // if set to tweet age is set and greater than that, exclude
-              if ( (theConfig.maxTweetAgeMins > 0) && 
-                    (cTweetAgeMins > theConfig.maxTweetAgeMins) )
-                    doInclude = false;
-              // if set to check for certain text matches, exclude those that don't
-              if ( (theConfig.excludeTweetsWithoutText.length > 0) &&
-            		  (this.doesNotHaveRequiredText(cText, theConfig)) )
-            	  	doInclude = false;
-              // if not included for some reason, include it
-              if (doInclude)
-              {
-                  // build tweet info for module
-                var cTweetInfo = { 
+    parseTweets: function (theConfig, tweets) {
+
+        var includedTweetList = [];
+        var userTweetCountList = {};
+        var nowTime = Date.now();
+
+        console.log(this.name + " #### tweets.length " + tweets.length);
+
+        for (var cIndex = 0; cIndex < tweets.length; cIndex++) {
+            // break out all the tweet components
+            var cTweet = tweets[cIndex];
+            var cText = this.cleanString(cTweet.text, theConfig);
+            var cTimestamp = cTweet.created_at;
+            var cTweetAgeMins = Math.round((nowTime - Date.parse(cTimestamp)) / (1000 * 60));
+            var cUserFullName = this.cleanString(cTweet.user.name, theConfig);
+            var cUserScreenName = cTweet.user.screen_name;
+            var cIsQuoteStatus = cTweet.is_quote_status;
+            var cIsRetweeted = (cTweet.retweeted_status !== undefined);
+            var cHasMedia = (cTweet.entities.media !== undefined);
+            var cHasURLs = (cTweet.entities.urls.length !== 0);
+            if (userTweetCountList[cUserScreenName] === undefined)
+                userTweetCountList[cUserScreenName] = { count: 1 };
+            else
+                userTweetCountList[cUserScreenName].count += 1;
+            var cUserTweetCount = userTweetCountList[cUserScreenName].count;
+            // set flag to assume inclusion of this tweet
+            var doInclude = true;
+
+
+            // if set to exclude quote and has a quote, exclude
+            if (theConfig.excludeTweetsWithQuotes && cIsQuoteStatus)
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 1" + doInclude); 
+            // if set to exclude retweets and is a retweet, exclude
+            if (theConfig.excludeRetweets && cIsRetweeted)
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 2" + doInclude); 
+            // if set to exclude tweets with media has media, exclude
+            if (theConfig.excludeMediaTweets && cHasMedia)
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 3" + doInclude); 
+            // if set to exclude tweets with links and has a link, exclude
+            if (theConfig.excludeLinkTweets && cHasURLs)
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 4" + doInclude); 
+            // if set to exclude short tweets and is short, exclude
+            if ((theConfig.excludeTweetLengthLessThan > 0) &&
+                (cText.length < theConfig.excludeTweetLengthLessThan))
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 5" + doInclude); 
+            // if set to limit tweets per user check if exceeds that amount, exclude
+            if ((theConfig.maxTweetsPerUser > 0) &&
+                (cUserTweetCount > theConfig.maxTweetsPerUser))
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 6" + doInclude); 
+            // if set to tweet age is set and greater than that, exclude
+            if ((theConfig.maxTweetAgeMins > 0) &&
+                (cTweetAgeMins > theConfig.maxTweetAgeMins))
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 7" + doInclude); 
+            // if set to check for certain text matches, exclude those that don't
+            if ((theConfig.excludeTweetsWithoutText.length > 0) &&
+                (this.doesNotHaveRequiredText(cText, theConfig)))
+                doInclude = false;
+            //console.log(this.name + " #### doInclude 8" + doInclude); 
+
+            // if not included for some reason, include it
+            if (doInclude) {
+                // build tweet info for module
+                var cTweetInfo = {
                     text: cText,
-                    screen_name: cUserScreenName,                                
-                    name: cUserFullName,  
-                    timestamp: cTimestamp,  
+                    screen_name: cUserScreenName,
+                    name: cUserFullName,
+                    timestamp: cTimestamp,
                     index: includedTweetList.length,
-                };  
+                };
                 // add tweet to the list
                 includedTweetList[includedTweetList.length] = cTweetInfo;
-              }                                                 
-          }  
-          // build payload, identified and tweet list
-          var returnPayload = { identifier: theConfig.identifier,
-                                tweetList: includedTweetList};
-          // send payload to module
-          this.sendSocketNotification('TWEETS_UPDATE', returnPayload);
+            }
+        }
+        // build payload, identified and tweet list
+        var returnPayload = {
+            identifier: theConfig.identifier,
+            tweetList: includedTweetList
+        };
+        // send payload to module
+        this.sendSocketNotification('TWEETS_UPDATE', returnPayload);
     },
     // main helper function to get the trends
-    updateTweets: function(theConfig) { 
+
+
+    //search example
+    //client.get('search/tweets', { q: 'node.js' }, function (error, tweets, response) {
+    //    console.log(tweets);
+    //});
+
+    updateTweets: function (theConfig) {
         var client = new Twitter({
-          consumer_key: theConfig.consumer_key,
-          consumer_secret: theConfig.consumer_secret,
-          access_token_key: theConfig.access_token_key,
-          access_token_secret: theConfig.access_token_secret
-        });  
+            consumer_key: theConfig.consumer_key,
+            consumer_secret: theConfig.consumer_secret,
+            access_token_key: theConfig.access_token_key,
+            access_token_secret: theConfig.access_token_secret
+        });
         // this to self
         var self = this;
         // prepare the twitter client param, clear query and params
         var query = '';
-        var params = { };
+        var params = {};
+
         // if no list name or timeline, set for timeline get
-        if ((theConfig.listToShow === '') 
-                || (theConfig.listToShow.toUpperCase() === 'TIMELINE')) {
-            query = 'statuses/home_timeline';
-            params = { count: theConfig.totalTweetsPerUpdate, 
-                screen_name: theConfig.screenName,
-                exclude_replies: true, include_rts: false };
+        if ((theConfig.listToShow.toUpperCase() === 'SEARCH')) {
+            query = 'search/tweets';
+            params = {
+                q: theConfig.screenName,
+                count: theConfig.totalTweetsPerUpdate
+            };
         }
-        // otherwise get a named list
         else {
-            query = 'lists/statuses';
-            params = { count: theConfig.totalTweetsPerUpdate, exclude_replies: true, include_rts: false, 
-                slug: theConfig.listToShow, owner_screen_name: theConfig.screenName };  
+            // otherwise get a named list
+            if ((theConfig.listToShow === '') || (theConfig.listToShow.toUpperCase() === 'TIMELINE')) {
+                query = 'statuses/home_timeline';
+                params = {
+                    count: theConfig.totalTweetsPerUpdate,
+                    screen_name: theConfig.screenName,
+                    exclude_replies: true, include_rts: false
+                };
+            }
+            // otherwise get a named list
+            else {
+                query = 'lists/statuses';
+                params = {
+                    count: theConfig.totalTweetsPerUpdate, exclude_replies: true, include_rts: false,
+                    slug: theConfig.listToShow, owner_screen_name: theConfig.screenName
+                };
+            }
         }
         // call twitter client based on query and params
-        client.get(query, params, function(error, tweets, response) {
+
+        client.get(query, params, function (error, tweets, response) {
             // if no error, send tweets for processing
+
             if (!error) {
+
+                //if we are searching we need a little preprocessing
+
+                if ((theConfig.listToShow.toUpperCase() === 'SEARCH')) {
+                    tweets = tweets['statuses'];
+                }
+
                 self.parseTweets(theConfig, tweets);
             }
             // otherwise process error
             else {
                 self.processError();
             }
-        });  
-    },      
+        });
+    },
 });
 
 //------------ end -------------
